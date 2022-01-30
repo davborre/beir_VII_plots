819a7402659968f8ec9a6bb92ac040860f8268ce
David Borrego <cookiesandbooks@icloud.com>
2022-01-30 17:37:12 -0500

    both EAR and ERR are generated now

--- beir_analysis.m
+++ beir_analysis.m
@@ -14,7 +14,7 @@ dose = 1; % radiation dose (Sv)
 %% import other risk parameters; See BEIR VII, Pg. XX, Table XX
 load ERR_EAR_parameters
 
-%% Generate ERR and EAR arrays
+%% Generate ERR and EAR data for plotting
 
 beta_f = ERR.IR.BetaF(1);
 beta_m = ERR.IR.BetaM(1);
@@ -22,6 +22,7 @@ beta_m = ERR.IR.BetaM(1);
 eta    = ERR.IR.Eta(1);
 gamma  = ERR.IR.Gamma(1);
 
+% ERR Section
 % sex averaged ERR structure for various ages
 % data(i).name is used as for legend and to indicate groups
 ERR_data(1).name = 'Age at exposure 30+';
@@ -38,7 +39,27 @@ ERR_data(3).name = 'Age at exposure 10';
 age_exp      = 10;
 [ERR_data(3).risk,ERR_data(3).age] = risk_model(...
                                 (beta_m+beta_f)/2,eta,gamma,dose,age_exp);
-                            
+% EAR SECTION
+beta_f = EAR.IR.BetaF(1);
+beta_m = EAR.IR.BetaM(1);
+
+eta    = EAR.IR.Eta(1);
+gamma  = EAR.IR.Gamma(1);
+
+EAR_data(1).name = 'Age at exposure 30+';
+age_exp      = 30;
+[EAR_data(1).risk,EAR_data(1).age] = risk_model(...
+                                (beta_m+beta_f)/2,eta,gamma,dose,age_exp);
+
+EAR_data(2).name = 'Age at exposure 20';
+age_exp      = 20;
+[EAR_data(2).risk,EAR_data(2).age] = risk_model(...
+                                (beta_m+beta_f)/2,eta,gamma,dose,age_exp);
+
+EAR_data(3).name = 'Age at exposure 10';
+age_exp      = 10;
+[EAR_data(3).risk,EAR_data(3).age] = risk_model(...
+                                (beta_m+beta_f)/2,eta,gamma,dose,age_exp);
 
 %% Calling plotting functions
 filename = sprintf('%s_%s_%s',yyyy_mm_dd,git_short_hash,'ERR');
@@ -58,10 +79,52 @@ for i = 1:numel(ERR_data)
     group = [group;temp_group];
 end
 
+ylim         = [0.2 2.4];
+ytick        = 0.2:0.2:2.4;
+leg_position = [0.6 0.6 .1 .3];
 linear_plotter(...
-    filename,xdata,ydata,group,'Attained age','Excess Relative Risk (1 Sv)','test')
+    filename,...
+    xdata,...
+    ydata,...
+    group,...
+    'Attained age',...
+    'Excess Relative Risk (1 Sv)',...
+    '',...
+    ylim,...
+    ytick,...
+    leg_position);
 
+filename = sprintf('%s_%s_%s',yyyy_mm_dd,git_short_hash,'EAR');
 
+xdata = [];
+ydata = [];
+group = [];
+
+for i = 1:numel(EAR_data)
+    num_datapts = numel(EAR_data(i).risk);
+    
+    xdata = [xdata;EAR_data(i).age];
+    ydata = [ydata;EAR_data(i).risk];
+    
+    temp_group    = cell(num_datapts,1);
+    temp_group(:) = {EAR_data(i).name};
+    group = [group;temp_group];
+end
+
+ylim = [0 75];
+ytick = 0:10:70;
+leg_position = [0.2 0.6 .1 .3];
+linear_plotter(...
+    filename,...
+    xdata,...
+    ydata,...
+    group,...
+    'Attained age',...
+    'Excess cases per 10,000 PY-Sv',...
+    '',...
+    ylim,...
+    ytick,...
+    leg_position)
 %% ERR and EAR models functions
 function [IR,age_attained]=risk_model(beta,eta,gamma,dose,age_exposed)
 %{
